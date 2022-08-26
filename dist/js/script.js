e6fd90698eed918aca5a5a65f8c5c75f43fa3325
Kamil <isaevkamil029@mail.ru>
2022-08-26 17:00:30 +0300

    add delivery and contacts

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,4 +6,31 @@ $(document).ready(function(){
         loop: true,
         dotsEach: 3,
     });
+
+
+
+    //SCROLL
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1800) {
+            $('.arrowUp').fadeIn();
+        } else {
+            $('.arrowUp').fadeOut();
+        }
+    });
+    
+    
+    $("a[href^='#up']").click(function() {
+    
+        const _href = $(this).attr("href");
+    
+            $('html, body').animate({
+            scrollTop: $(_href).offset().top+"px"});
+            return false;
+        });
+    
 });
+
+
+
+
