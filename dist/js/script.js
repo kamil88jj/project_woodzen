0ace6c2c8fcfa6c16bdfdd271c7b57c234a52fcb
Kamil <isaevkamil029@mail.ru>
2022-09-02 17:27:11 +0300

    add price

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         }
     });
     
-    
+    //Arrow Up
     $("a[href^='#up']").click(function() {
     
         const _href = $(this).attr("href");
@@ -25,6 +25,23 @@ $(document).ready(function(){
             scrollTop: $(_href).offset().top+"px"});
             return false;
         });
+
+
+    //adding prices
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
     
 });
 
